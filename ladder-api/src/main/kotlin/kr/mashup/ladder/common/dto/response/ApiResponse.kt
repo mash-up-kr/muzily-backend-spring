da63b44360899f6f4b725e85fdde56cd02633c97
seungh0 <will.seungho@gmail.com>
2022-06-01 03:24:20 +0900

    feat: ControllerException Handler 추가

--- ladder-api/src/main/kotlin/kr/mashup/ladder/common/dto/response/ApiResponse.kt
+++ ladder-api/src/main/kotlin/kr/mashup/ladder/common/dto/response/ApiResponse.kt
@@ -1,16 +1,27 @@
 package kr.mashup.ladder.common.dto.response
 
+import kr.mashup.ladder.domain.common.error.ErrorCode
+
 data class ApiResponse<T>(
     val code: String = "",
-    val message: String = "",
+    val message: String = "", // TODO: message 서버에서 내려줄 건지 여부 확인 필요
     val data: T?,
 ) {
 
     companion object {
         val OK = ApiResponse(data = "OK")
+
         fun <T> ok(data: T): ApiResponse<T> {
             return ApiResponse(
                 data = data
+            )
+        }
+
+        fun error(errorCode: ErrorCode): ApiResponse<Nothing> {
+            return ApiResponse(
+                code = errorCode.code,
+                message = errorCode.message,
+                data = null
             )
         }
     }
---
+++ ladder-domain/src/main/kotlin/kr/mashup/ladder/domain/common/utils/LoggerUtils.kt
@@ -0,0 +1,8 @@
+package kr.mashup.ladder.domain.common.utils
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+inline fun <reified T> T.logger(): Logger {
+    return LoggerFactory.getLogger(T::class.java)
+}
