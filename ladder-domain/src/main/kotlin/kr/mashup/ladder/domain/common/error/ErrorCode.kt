ec9deb6c9c6e5e8fcd5d69919f941049bb10a679
seungh0 <will.seungho@gmail.com>
2022-06-01 03:23:50 +0900

    feat: 커스텀 Exception 모델 정의

--- ladder-domain/src/main/kotlin/kr/mashup/ladder/domain/common/error/ErrorCode.kt
+++ ladder-domain/src/main/kotlin/kr/mashup/ladder/domain/common/error/ErrorCode.kt
@@ -6,20 +6,21 @@ package kr.mashup.ladder.domain.common.error
  */
 
 enum class ErrorCode(
-    val code: String,
     val status: Int, // TODO: HttpStatus 로 변환 고려
+    val code: String,
+    val message: String,
     val shouldLog: Boolean,
 ) {
     // common
-    INVALID_REQUEST("C001", 400, false),
-    UNAUTHORIZED("C002", 401, false),
-    FORBIDDEN("C003", 403, false),
-    NOT_FOUND("C004", 404, false),
-    CONFLICT("C005", 409, false),
-    UNKNOWN_ERROR("C006", 500, true),
+    INVALID_REQUEST(status = 400, code = "C001", message = "잘못된 요청", shouldLog = false),
+    UNAUTHORIZED(status = 401, code = "C002", message = "인증 실패", shouldLog = false),
+    FORBIDDEN(status = 403, code = "C003", message = "인가 실패", shouldLog = false),
+    NOT_FOUND(status = 404, code = "C004", message = "해당하는 리소스는 존재하지 않습니다", shouldLog = false),
+    CONFLICT(status = 409, code = "C005", message = "중복된 리소스가 존재합니다", shouldLog = false),
+    UNKNOWN_ERROR(status = 500, code = "C006", message = "서버에서 에러가 발생하였습니다 ㅠㅠ", shouldLog = true),
 
     // room
-    ROOM_NOT_FOUND("R001", 404, false),
+    ROOM_NOT_FOUND(status = 404, code = "R001", message = "해당하는 방이 존재하지 않습니단", shouldLog = false),
 
     // ...
 }
