74df371826b49a26492efe7fc140be3c1e2c0162
seungh0 <will.seungho@gmail.com>
2022-06-22 02:26:56 +0900

    fix: kotest -> junit으로 수정

--- ladder-api/src/test/kotlin/kr/mashup/ladder/IntegrationTest.kt
+++ ladder-api/src/test/kotlin/kr/mashup/ladder/IntegrationTest.kt
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.server.LocalServerPort
 import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.TestConstructor
 
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 abstract class IntegrationTest {
