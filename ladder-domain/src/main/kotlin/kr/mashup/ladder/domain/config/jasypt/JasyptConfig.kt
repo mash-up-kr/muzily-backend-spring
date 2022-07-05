d29b1ebd3ff866d10370b3b109c620e05d5dad25
y2o2u2n <y2o2u2n@gmail.com>
2022-07-05 23:22:06 +0900

    feat: 설정 정보 관리 (#32)
    * feat: jasypt config
    
    - https://github.com/ulisesbocchio/jasypt-spring-boot
    
    * feat: 카카오톡 설정값 암호화
    
    * feat: 배포시 브랜치 선택 옵션 재추가
    
    * fix: 테스트 실패 수정
    
    * feat: secret yml 에 jasypt.encryptor.password 명시하여 사용

---
+++ ladder-domain/src/main/kotlin/kr/mashup/ladder/domain/config/jasypt/JasyptConfig.kt
@@ -0,0 +1,9 @@
+package kr.mashup.ladder.domain.config.jasypt
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties
+import org.springframework.context.annotation.Configuration
+
+
+@Configuration
+@EnableEncryptableProperties
+class JasyptConfig
