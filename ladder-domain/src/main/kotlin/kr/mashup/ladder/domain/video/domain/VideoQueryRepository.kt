2a8c1d4dbeb29d828c95e15a04d3872d07592f1b
윤준석 <y2o2u2n@gmail.com>
2022-07-06 23:03:26 +0900

    chore: 샘플 코드 제거

--- ladder-domain/src/main/kotlin/kr/mashup/ladder/domain/video/domain/VideoQueryRepository.kt
+++
@@ -1,7 +0,0 @@
-package kr.mashup.ladder.domain.video.domain
-
-@Deprecated(message = "샘플 코드")
-interface VideoQueryRepository {
-
-    fun findAll(): List<Video>
-}
--- ladder-domain/src/main/kotlin/kr/mashup/ladder/domain/video/infra/jpa/VideoJpaRepository.kt
+++
@@ -1,8 +0,0 @@
-package kr.mashup.ladder.domain.video.infra.jpa
-
-import kr.mashup.ladder.domain.video.domain.Video
-import kr.mashup.ladder.domain.video.domain.VideoRepository
-import org.springframework.data.jpa.repository.JpaRepository
-
-@Deprecated(message = "샘플 코드")
-interface VideoJpaRepository : JpaRepository<Video, String>, VideoRepository
