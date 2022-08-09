4de1023482b469def1d6e5193d8bfe61ba157a88
seungh0 <will.seungho@gmail.com>
2022-08-10 01:56:24 +0900

    feat: 개발/운영 프론트 도메인에 대한 CORS 추가

--- ladder-search/src/main/kotlin/kr/mashup/ladder/config/webclient/WebConfig.kt
+++ ladder-search/src/main/kotlin/kr/mashup/ladder/config/webclient/WebConfig.kt
@@ -9,7 +9,11 @@ class WebConfig : WebFluxConfigurer {
 
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000") // TODO: 프론트에서 proxy 설정하면 localhost 제거
+            .allowedOrigins(
+                "http://localhost:3000",
+                "https://developmusily.netlify.app",
+                "https://musily.netlify.app"
+            ) // TODO: 프론트에서 proxy 설정하면 localhost 제거
             .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS", "HEAD")
             .allowedHeaders("*")
             .allowCredentials(true)
