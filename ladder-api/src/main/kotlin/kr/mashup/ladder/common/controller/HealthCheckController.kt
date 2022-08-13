997f4a2ff1edc051d91d217159d9dcae51bb1ee6
seungh0 <will.seungho@gmail.com>
2022-08-13 20:14:13 +0900

    feat: Liveness, Readiness API 추가

--- ladder-api/src/main/kotlin/kr/mashup/ladder/common/controller/HealthCheckController.kt
+++ ladder-api/src/main/kotlin/kr/mashup/ladder/common/controller/HealthCheckController.kt
@@ -1,14 +1,41 @@
 package kr.mashup.ladder.common.controller
 
+import kr.mashup.ladder.domain.common.constants.ApiResponseConstants.SUCCESS
+import kr.mashup.ladder.domain.common.exception.model.BadGatewayException
+import org.springframework.boot.availability.ApplicationAvailability
+import org.springframework.boot.availability.LivenessState
+import org.springframework.boot.availability.ReadinessState
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+@RequestMapping("/api")
 @RestController
-class HealthCheckController {
+class HealthCheckController(
+    private val applicationAvailability: ApplicationAvailability,
+) {
 
-    @GetMapping("/api/health")
+    @GetMapping("/health")
     fun health(): String {
-        return "OK"
+        return SUCCESS
+    }
+
+    @GetMapping("/liveness")
+    fun liveness(): String {
+        val livenessState = applicationAvailability.livenessState
+        if (livenessState != LivenessState.CORRECT) {
+            throw BadGatewayException("Server is not in liveness")
+        }
+        return SUCCESS
+    }
+
+    @GetMapping("/readiness")
+    fun readiness(): String {
+        val readinessState = applicationAvailability.readinessState
+        if (readinessState != ReadinessState.ACCEPTING_TRAFFIC) {
+            throw BadGatewayException("Server is not in readiness")
+        }
+        return SUCCESS
     }
 
 }
