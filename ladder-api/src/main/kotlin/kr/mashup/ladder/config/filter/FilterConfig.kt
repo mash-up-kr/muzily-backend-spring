c2718885754fbaa9c894632a073d1881b181981a
seungh0 <will.seungho@gmail.com>
2022-08-20 19:30:44 +0900

    feat: Request, Response 로깅 추가

---
+++ ladder-api/src/main/kotlin/kr/mashup/ladder/config/filter/FilterConfig.kt
@@ -0,0 +1,20 @@
+package kr.mashup.ladder.config.filter
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+
+@Configuration
+class FilterConfig {
+
+    @Profile("dev", "prod")
+    @Bean
+    fun requestLoggingFilter(): FilterRegistrationBean<RequestLoggingFilter> {
+        val filter: FilterRegistrationBean<RequestLoggingFilter> = FilterRegistrationBean(RequestLoggingFilter())
+        filter.addUrlPatterns("/api/v1/*", "/ws/*")
+        filter.order = 1
+        return filter
+    }
+
+}
