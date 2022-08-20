b4fe97e64db5b735a7f421ffb4cc79d01492a177
seungh0 <will.seungho@gmail.com>
2022-08-21 00:06:08 +0900

    feat: 방 초대장 정보에 플레이리스트 아이템 갯수도 포함하도록 수정

--- ladder-api/src/main/kotlin/kr/mashup/ladder/room/service/RoomInviteService.kt
+++ ladder-api/src/main/kotlin/kr/mashup/ladder/room/service/RoomInviteService.kt
@@ -1,5 +1,6 @@
 package kr.mashup.ladder.room.service
 
+import kr.mashup.ladder.domain.playlist.domain.PlaylistRepository
 import kr.mashup.ladder.domain.room.infra.jpa.RoomRepository
 import kr.mashup.ladder.room.dto.response.RoomInfoResponse
 import kr.mashup.ladder.room.service.RoomServiceHelper.findRoomByInvitationKey
@@ -9,12 +10,16 @@ import org.springframework.transaction.annotation.Transactional
 @Service
 class RoomInviteService(
     private val roomRepository: RoomRepository,
+    private val playlistRepository: PlaylistRepository,
 ) {
 
     @Transactional(readOnly = true)
     fun getByInvitationKey(invitationKey: String, memberId: Long): RoomInfoResponse {
         val room = findRoomByInvitationKey(roomRepository, invitationKey)
-        return RoomInfoResponse.from(room = room, memberId = memberId)
+        return RoomInfoResponse.of(
+            room = room, memberId = memberId,
+            playListItemsCount = playlistRepository.findByRoomId(room.id)?.items?.size ?: 0
+        )
     }
 
     @Transactional
